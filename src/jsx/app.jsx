978416f8efa6959318e58adbb9b402827013d087
Nolan Kovacik <noltron000@gmail.com>
2019-02-27 16:11:14 -0800

    🏗 setting up some serious foundation for future additions

---
+++ src/jsx/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css/main.css';
+// import Placeholder from '../jsx/placeholder.jsx';
+
+class App extends Component {
+	render() {
+		return (
+			<div id='root'>
+				<h1>Hello, world!</h1>
+				{/* <Placeholder /> */}
+			</div>
+		);
+	}
+}
+
+export default App;
