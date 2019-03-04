cb0714ca332e83008e1a3682093707121af4a17c
Nolan Kovacik <noltron000@gmail.com>
2019-03-04 15:04:15 -0800

    🎛 resizing assets

--- src/jsx/avatar.jsx
+++ src/jsx/avatar.jsx
@@ -8,9 +8,9 @@ class Avatar extends Component {
 			<div id='avatar'>
 				<h2>Here's your avatar:</h2>
 				<div className='container'>
-					<img alt='body' src={require(`../img/accessories/bodies/${this.props.items.bodies}`)} className='avatar-item' />
-					<img alt='eyes' src={require(`../img/accessories/eyes/${this.props.items.eyes}`)} className='avatar-item' />
-					<img alt='hat' src={require(`../img/accessories/hats/${this.props.items.hats}`)} className='avatar-item' />
+					<img alt='body' src={require(`../img/accessories/bodies/${this.props.items.bodies}`)} className='avatar-item body' />
+					<img alt='eyes' src={require(`../img/accessories/eyes/${this.props.items.eyes}`)} className='avatar-item eye' />
+					<img alt='hat' src={require(`../img/accessories/hats/${this.props.items.hats}`)} className='avatar-item hat' />
 				</div>
 			</div >
 		);
