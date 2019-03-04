b651642bebfa9ec6afb6c06036c9a54baeb2bcb5
Nolan Kovacik <noltron000@gmail.com>
2019-03-04 11:11:13 -0800

    rip

---
+++ src/jsx/avatar.jsx
@@ -0,0 +1,17 @@
+// import react package
+import React, { Component } from 'react';
+
+class Avatar extends Component {
+	render() {
+		return (
+			<div id='avatar'>
+				<h2>Here's your avatar:</h2>
+				<div>
+					<h3>WOAH! An output will go here.</h3>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Avatar;
---
+++ src/jsx/widget.jsx
@@ -0,0 +1,105 @@
+// import react package
+import React, { Component } from 'react';
+
+// import components
+import EquipSlot from '../jsx/equip-slot.jsx';
+
+class ChoiceWidget extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			ItemData: {
+				hats: [
+					'hat01.png',
+					'hat02.png',
+					'hat03.png'
+				],
+				eyes: [
+					'eye01.png',
+					'eye02.png',
+					'eye03.png'
+				],
+				bodies: [
+					'llamacorn01.png',
+					'llamacorn02.jpg',
+					'llamacorn03.png'
+				]
+			}
+		}
+	}
+
+	renderEquipSlot() {
+		let slotList = []
+
+		for (let slot in this.state.ItemData) {
+			slotList.push(slot)
+		}
+
+		console.log(slotList)
+
+		return (
+			<div>
+				{slotList.map((slot) => <EquipSlot key={slot} slot={slot} ItemData={this.state.ItemData} />)}
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<div id='widget'>
+				<h2>Here's your widgets:</h2>
+				{/* this will be a selection of accessories for this equip slot */}
+				<form id='options'>
+					{this.renderEquipSlot()}
+				</form>
+				{/*
+					***IMPORTANT***
+					This is where a valuable object will be passed in.
+					The object will tell the app where images are located,
+					what the names of the UI assets are to display,
+					and how many radio buttons and widgets it should render.
+
+					It will look like this:
+					{
+						hats: [
+							'hat01.png',
+							'hat02.png',
+							'hat03.png'
+						],
+						eyes: [
+							'eye01.png',
+							'no-naming-convention.png',
+							'any-filetype.jpg'
+						],
+						bodies: [
+							'body01.png',
+							'body02.jpg',
+							'body08.png'
+						]
+					}
+				*/}
+			</div>
+		);
+	}
+}
+
+export default ChoiceWidget;
+
+// // Hats.js
+// export default [
+// 	a:1
+// ]
+
+// // faces.js
+// export default [
+// 	a: 1
+// ]
+
+// // index.js
+// import hats from 'hats'
+// import faces from 'faces'
+
+// export default {
+// 	hats,
+// 	faces
+// }
