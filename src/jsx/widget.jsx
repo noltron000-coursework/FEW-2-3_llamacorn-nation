05189a68dda4664942dcc14ba9b9f54cfd6dca71
Nolan Kovacik <noltron000@gmail.com>
2019-05-30 11:41:06 -0700

    late commit

--- src/jsx/widget.jsx
+++ src/jsx/widget.jsx
@@ -12,17 +12,20 @@ class ChoiceWidget extends Component {
 				hats: [
 					'hat01.png',
 					'hat02.png',
-					'hat03.png'
+					'hat03.png',
+					'null'
 				],
 				eyes: [
 					'eye01.png',
 					'eye02.png',
-					'eye03.png'
+					'eye03.png',
+					'null'
 				],
 				bodies: [
 					'llamacorn01.png',
 					'llamacorn02.jpg',
-					'llamacorn03.png'
+					'llamacorn03.png',
+					'null'
 				]
 			},
 			ChoiceData: {}
